1a67d7912b09a8e723b1fa3537ce502109023786
Guy Korland <gkorland@gmail.com>
2019-03-18 17:33:13 +0200

    add delete relation on update

--- script.sh
+++ script.sh
@@ -52,7 +52,7 @@ redis-cli RG.PYEXECUTE "GearsBuilder().foreach(lambda x: execute('GRAPH.QUERY', 
 
 redis-cli RG.PYEXECUTE "GearsBuilder().filter(lambda x: 'Married' in x['value'].keys()).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (a:marsman), (b:marsman) where a.ID=\"%s\" AND b.ID=\"%s\" CREATE (a)-[:Married]->(b)' % (x['key'], x['value']['Married']))).run('marsman:*')"
 
-redis-cli RG.PYEXECUTE "GearsBuilder().foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MERGE ( :marsman {ID:\"%s\"})' % x['key'])).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen','MATCH ( n:marsman {ID:\"%s\"}) SET n.Name=\"%s\", n.Last=\"%s\"' % (x['key'], x['value']['Name'], x['value']['Last']))).filter(lambda x: 'Married' in x['value'].keys()).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (a:marsman), (b:marsman) where a.ID=\"%s\" AND b.ID=\"%s\" CREATE (a)-[:Married]->(b)' % (x['key'], x['value']['Married']))).count().register('marsman:*')"
+redis-cli RG.PYEXECUTE "GearsBuilder().foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MERGE ( :marsman {ID:\"%s\"})' % x['key'])).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen','MATCH ( n:marsman {ID:\"%s\"}) SET n.Name=\"%s\", n.Last=\"%s\"' % (x['key'], x['value']['Name'], x['value']['Last']))).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (a:marsman)-(r)->() where a.ID=\"%s\" DELETE r' % (x['key']))).filter(lambda x: 'Married' in x['value'].keys()).foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (a:marsman), (b:marsman) where a.ID=\"%s\" AND b.ID=\"%s\" CREATE (a)-[:Married]->(b)' % (x['key'], x['value']['Married']))).count().register('marsman:*')"
 
 read
 #########################################################################################################
