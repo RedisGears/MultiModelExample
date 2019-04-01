d89ea6e0c1b4e093bb0767e814882ad02d914411
Guy Korland <gkorland@gmail.com>
2019-03-31 19:37:57 -0700

    add promt on read

--- script.sh
+++ script.sh
@@ -6,8 +6,9 @@ read
 
 redis-cli HSET marsman:100 Name Douglas Last Quaid
 redis-cli HSET marsman:101 Name Lori Last Quaid       # Quaid's seemingly loving wife
+redis-cli HSET marsman:102 Name Melina Last Melina 
 
-read
+read -p "Back to slides..."
 
 ######## RediSearch ################################################################################################# 
 
@@ -38,14 +39,14 @@ redis-cli FT.SEARCH marsmen Qu*
 
 read
 
-redis-cli HSET marsman:102 Name Vilos Last Cohaagen # Governor of the Mars 
-redis-cli HSET marsman:103 Name Bob Last McClane    # Rekall manager and sales agent 
+redis-cli HSET marsman:103 Name Vilos Last Cohaagen # Governor of the Mars 
+redis-cli HSET marsman:104 Name Bob Last McClane    # Rekall manager and sales agent 
 
 read
 
 redis-cli FT.SEARCH marsmen mcc*
 
-read
+read -p "Back to slides..."
 
 ######## RedisGraph ################################################################################################# 
 
@@ -60,8 +61,7 @@ read
 # Create nodes for all marsmen
 redis-cli RG.PYEXECUTE \
 "GearsBuilder()\
-.foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MERGE ( :marsman {ID:\"%s\"})' % x['key']))\
-.foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (n:marsman {ID:\"%s\"}) SET n.Name=\"%s\", n.Last=\"%s\"' % (x['key'], x['value']['Name'], x['value']['Last'])))\
+.foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MERGE (n:marsman {ID:\"%s\", Name:\"%s\", Last:\"%s\"})' % (x['key'], x['value']['Name'], x['value']['Last'])))\
 .count()\
 .run('marsman:*')"
 
@@ -70,6 +70,7 @@ redis-cli RG.PYEXECUTE \
 "GearsBuilder()\
 .filter(lambda x: 'Relation' in x['value'].keys())\
 .foreach(lambda x: execute('GRAPH.QUERY', 'marsmen', 'MATCH (a:marsman), (b:marsman) where a.ID=\"%s\" AND b.ID=\"%s\" CREATE (a)-[:Relation]->(b)' % (x['key'], x['value']['Relation'])))\
+.count()\
 .run('marsman:*')"
 
 # Register for new nodes/relations update
@@ -88,21 +89,23 @@ redis-cli GRAPH.QUERY marsmen "MATCH (n)-[:Relation]->(m) return n.Name, m.Name"
 
 read
 
-redis-cli HSET marsman:104 Name Robert Last Costanzo Relation marsman:100 # Quaid's workmate (Cohaagen's agent)
+redis-cli HSET marsman:104 Relation marsman:100 # Quaid's workmate (Cohaagen's agent)
 
 read
 
 redis-cli GRAPH.QUERY marsmen "MATCH (n)-[:Relation]->(m) return n.Name, m.Name"
 
 
-read
+read -p "Back to slides..."
 
-redis-cli HDEL marsman:104 Relation # Quaid killed! Robert
+redis-cli HDEL marsman:101 Relation # Quaid killed! Lori 
 
 read
 
 redis-cli GRAPH.QUERY marsmen "MATCH (n)-[:Relation]->(m) return n.Name, m.Name"
- 
+
+read -p "Back to slides..."
+
 ######## RedisTimeSeries ############################################################################################ 
 
 redis-cli MODULE LOAD `pwd`/redistimeseries.so
@@ -132,3 +135,5 @@ done
 read
 
 redis-cli TS.RANGE heartbeat:marsman:100 - + AGGREGATION AVG 5
+
+read -p "Back to slides..."
